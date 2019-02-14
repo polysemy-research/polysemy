582578b3cdf72985cdf1a55439c1a95ffcc061ad
Sandy Maguire <sandy@sandymaguire.me>
2019-02-13 21:58:48 -0500

    aggressively inline

--- src/Lib.hs
+++ src/Lib.hs
@@ -16,7 +16,7 @@ import Data.Functor.Compose
 import Data.IORef
 import Control.Arrow (second)
 import System.IO.Unsafe
-import qualified Control.Monad.Trans.State as S
+import qualified Control.Monad.Trans.State.Strict as S
 import Data.Functor.Identity
 import Control.Monad.Trans (lift)
 
@@ -27,22 +27,29 @@ newtype Freer f a = Freer
 
 freeMap :: (f ~> g) -> Freer f ~> Freer g
 freeMap nat (Freer m) = Freer $ \k -> m $ k . nat
+{-# INLINE freeMap #-}
 
 hoistEff :: Union r x -> Eff r x
 hoistEff u = Freer $ \k -> k u
+{-# INLINE hoistEff #-}
 
 instance Functor (Freer f) where
   fmap f (Freer z) = Freer $ \z' -> fmap f $ z z'
+  {-# INLINE fmap #-}
 
 instance Applicative (Freer f) where
   pure a = Freer $ const $ pure a
+  {-# INLINE pure #-}
   Freer f <*> Freer a = Freer $ \k -> f k <*> a k
+  {-# INLINE (<*>) #-}
 
 instance Monad (Freer f) where
   return = pure
+  {-# INLINE return #-}
   Freer ma >>= f = Freer $ \k -> do
     z <- ma k
     runFreer (f z) k
+  {-# INLINE (>>=) #-}
 
 
 type Eff r = Freer (Union r)
@@ -50,13 +57,16 @@ type Eff r = Freer (Union r)
 
 send :: Member eff r => eff a -> Eff r a
 send t = Freer $ \k -> k $ inj t
+{-# INLINE send #-}
 
 
 runM :: Monad m => Freer (Union '[m]) a -> m a
 runM z = runFreer z extract
+{-# INLINE runM #-}
 
 run :: Freer (Union '[Identity]) a -> a
 run = runIdentity . runM
+{-# INLINE run #-}
 
 
 data State s a where
@@ -65,9 +75,11 @@ data State s a where
 
 get :: Member (State s) r => Eff r s
 get = send Get
+{-# INLINE get #-}
 
 put :: Member (State s) r => s -> Eff r ()
 put = send . Put
+{-# INLINE put #-}
 
 
 foom :: Eff '[State String, IO] String
@@ -85,6 +97,7 @@ interpret f (Freer m) = Freer $ \k -> m $ \u -> do
   case decomp u of
     Left x -> k x
     Right y -> runFreer (f y) k
+{-# INLINE interpret #-}
 
 
 runTeletype :: forall r a. Member IO r => Eff (State String ': r) a -> Eff r a
@@ -100,18 +113,21 @@ interpretState s m = flip S.evalStateT s $ runFreer m $ \u ->
   case decomp u of
     Left x  -> S.StateT $ \s -> fmap (, s) $hoistEff x
     Right y -> S.mapStateT (pure . runIdentity) y
+{-# INLINE interpretState #-}
 
 
-main :: IO ()
-main = runM (runState "fuck" foom) >>= print
+-- main :: IO ()
+-- main = runM (runState "fuck" foom) >>= print
 
 
 raise :: Eff r a -> Eff (u ': r) a
 raise = freeMap weaken
+{-# INLINE raise #-}
 
 
 introduce :: Eff (eff ': r) a -> Eff (eff ': u ': r) a
 introduce = freeMap (shuffle . weaken)
+{-# INLINE introduce #-}
 
 
 interpretS
@@ -127,6 +143,8 @@ interpretS f s = interpretState s . interpret bind . introduce
       let (s', e') = f s e
       send $ S.put @Identity s'
       raise e'
+    {-# INLINE bind #-}
+{-# INLINE interpretS #-}
 
 
 
@@ -136,4 +154,6 @@ runState = interpretS nat
     nat :: s -> State s x -> (s, Eff r x)
     nat s Get      = (s, pure s)
     nat _ (Put s') = (s', pure ())
+    {-# INLINE nat #-}
+{-# INLINE runState #-}
 
