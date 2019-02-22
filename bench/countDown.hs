daad00bb262a9098fa865c603193ada65355e610
Samuel Gélineau <gelisam+github@gmail.com>
2019-02-21 19:39:13 -0500

    clearer benchmark labels

--- bench/countDown.hs
+++ bench/countDown.hs
@@ -126,8 +126,8 @@ main :: IO ()
 main =
   defaultMain [
     bgroup "Countdown Bench" [
-        bench "freer.State"    $ whnf countDown 10000
-      , bench "mtl.State"      $ whnf countDownMTL 10000
-      , bench "tftf.State"     $ whnf TFTF.countDown 10000
+        bench "freer-simple"      $ whnf countDown 10000
+      , bench "mtl"               $ whnf countDownMTL 10000
+      , bench "too-fast-too-free" $ whnf TFTF.countDown 10000
     ]
   ]
