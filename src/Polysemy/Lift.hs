877371653182223b13c0fa0ed5ab96c297bb9202
Georgi Lyubenov <godzbanebane@gmail.com>
2019-07-05 23:42:17 +0300

    Add Polysemy.Lift module and runLift interpreter

---
+++ src/Polysemy/Lift.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Polysemy.Lift
+  ( -- * Effect
+    Lift (..)
+
+    -- * Actions
+  , lift
+
+    -- * Interpretations
+  , runLift
+
+    -- * Lifting monadic actions into effects
+  , sendM
+  ) where
+
+import           Polysemy
+import           Polysemy.Lift.Type (Lift(..))
+
+makeSem ''Lift
+
+------------------------------------------------------------------------------
+-- | Given a natural transform from @m1@ to @m2@
+-- run a @Lift m1@ effect by transforming it into a @Lift m2@ effect.
+runLift :: Member (Lift m2) r => (forall x. m1 x -> m2 x) -> Sem (Lift m1 ': r) a -> Sem r a
+runLift f = interpret $ sendM . f . unLift
+{-# INLINE runLift #-}
