238f2106c80116d88845034946dbb6f3d88e49c8
Georgi Lyubenov <godzbanebane@gmail.com>
2019-07-06 09:07:44 +0300

    Add explicit foralls and split type signature

--- src/Polysemy/Lift.hs
+++ src/Polysemy/Lift.hs
@@ -24,6 +24,11 @@ makeSem ''Lift
 -- run a @Lift m1@ effect by transforming it into a @Lift m2@ effect.
 --
 -- TODO(sandy): @since
-runLift :: Member (Lift m2) r => (forall x. m1 x -> m2 x) -> Sem (Lift m1 ': r) a -> Sem r a
+runLift
+    :: forall m1 m2 r a
+     . Member (Lift m2) r
+    => (forall x. m1 x -> m2 x)
+    -> Sem (Lift m1 ': r) a
+    -> Sem r a
 runLift f = interpret $ sendM . f . unLift
 {-# INLINE runLift #-}
