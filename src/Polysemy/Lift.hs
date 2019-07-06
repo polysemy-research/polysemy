96a5b8901ed6497d380a0999e2a9f15003a9db8c
Georgi Lyubenov <godzbanebane@gmail.com>
2019-07-06 09:04:48 +0300

    Add a Sandy reminder

--- src/Polysemy/Lift.hs
+++ src/Polysemy/Lift.hs
@@ -22,6 +22,8 @@ makeSem ''Lift
 ------------------------------------------------------------------------------
 -- | Given a natural transform from @m1@ to @m2@
 -- run a @Lift m1@ effect by transforming it into a @Lift m2@ effect.
+--
+-- TODO(sandy): @since
 runLift :: Member (Lift m2) r => (forall x. m1 x -> m2 x) -> Sem (Lift m1 ': r) a -> Sem r a
 runLift f = interpret $ sendM . f . unLift
 {-# INLINE runLift #-}
