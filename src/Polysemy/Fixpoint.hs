0b8dd171a964aebb50514dec9a6b96f05115445b
unknown <combiner8761@gmail.com>
2019-07-26 20:14:43 +0200

    Improve semantics of Fixpoint

--- src/Polysemy/Fixpoint.hs
+++ src/Polysemy/Fixpoint.hs
@@ -12,21 +12,46 @@ import Control.Monad.Fix
 import Polysemy
 import Polysemy.Internal.Fixpoint
 
-
 ------------------------------------------------------------------------------
 -- | Run a 'Fixpoint' effect purely.
+--
+-- __Note__: 'runFixpoint' operates under the assumption that any effectful
+-- state which can't be inspected using 'Polysemy.Inspector' can't contain any
+-- values. This is true for all interpreters featured in this package,
+-- and is presumably always true for any properly implemented interpreter.
+--
+-- If 'runFixpoint' throws an exception for you, then you're likely using
+-- some interpreter outside of this package that uses
+-- 'Polysemy.Internal.Union.weave' improperly.
+-- If this cannot possibly be the case, open an issue over at the
+-- GitHub repository.
 runFixpoint
     :: (∀ x. Sem r x -> x)
     -> Sem (Fixpoint ': r) a
     -> Sem r a
 runFixpoint lower = interpretH $ \case
   Fixpoint mf -> do
-    c <- bindT mf
-    pure $ fix $ lower . runFixpoint lower . c
-
+    c   <- bindT mf
+    s   <- getInitialStateT
+    ins <- getInspectorT
+    pure $ fix $ \fa ->
+      lower . runFixpoint lower . c $
+        maybe (bomb "runFixpoint") id (inspect ins fa) <$ s
+{-# INLINE runFixpoint #-}
 
 ------------------------------------------------------------------------------
 -- | Run a 'Fixpoint' effect in terms of an underlying 'MonadFix' instance.
+--
+-- __Note__: 'runFixpointM' operates under the assumption that any effectful
+-- state which can't be inspected using 'Polysemy.Inspector' can't contain any
+-- values. This is true for all interpreters featured in this package,
+-- and is presumably always true for any properly implemented interpreter.
+--
+-- If 'runFixpointM' throws an exception for you, then you're likely using
+-- some interpreter outside of this package that uses
+-- 'Polysemy.Internal.Union.weave' improperly.
+-- If this cannot possibly be the case, open an issue over at the
+-- GitHub repository.
 runFixpointM
     :: ( MonadFix m
        , Member (Embed m) r
@@ -36,6 +61,19 @@ runFixpointM
     -> Sem r a
 runFixpointM lower = interpretH $ \case
   Fixpoint mf -> do
-    c <- bindT mf
-    embed $ mfix $ lower . runFixpointM lower . c
+    c   <- bindT mf
+    s   <- getInitialStateT
+    ins <- getInspectorT
+    embed $ mfix $ \fa ->
+      lower . runFixpointM lower . c $
+        maybe (bomb "runFixpointM") id (inspect ins fa) <$ s
+{-# INLINE runFixpointM #-}
 
+
+bomb :: String -> a
+bomb str = error $
+    str ++ ": Uninspectable effectful state still\
+            \ contained an observable result.\
+            \ You're likely using an interpreter\
+            \ that uses 'weave' improperly.\
+            \ See documentation for more information."
