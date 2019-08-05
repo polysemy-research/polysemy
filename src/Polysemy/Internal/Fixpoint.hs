4617d2effd69d092ef2ffaaf965c070f641bacc6
unknown <combiner8761@gmail.com>
2019-08-05 10:48:16 +0200

    Revert "Hide visibility of Polysemy.Fixpoint.bomb, fix changelog"
    This reverts commit 5b043ed2151f0abdc6e8974c91f5546830dbeb2e.

--- src/Polysemy/Internal/Fixpoint.hs
+++ src/Polysemy/Internal/Fixpoint.hs
@@ -7,18 +7,3 @@ module Polysemy.Internal.Fixpoint where
 data Fixpoint m a where
   Fixpoint :: (a -> m a) -> Fixpoint m a
 
-
-------------------------------------------------------------------------------
--- | The error used in 'Polysemy.Fixpoint.runFixpoint' and
--- 'Polysemy.Fixpoint.runFixpointM' when the result of a failed computation
--- is recursively used and somehow visible. You may use this for your own
--- 'Fixpoint' interpreters. The argument should be the name of the interpreter.
-bomb :: String -> a
-bomb str = error $
-    str ++ ": Internal computation failed.\
-            \ This is likely because you have tried to recursively use\
-            \ the result of a failed computation in an action\
-            \ whose effect may be observed even though the computation failed.\
-            \ It's also possible that you're using an interpreter\
-            \ that uses 'weave' improperly.\
-            \ See documentation for more information."
