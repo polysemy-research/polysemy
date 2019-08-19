6653cc6d31c54eb946ede3177973bf0ee4f28994
unknown <combiner8761@gmail.com>
2019-08-19 23:47:44 +0200

    Removed insignificant hyperlinks

--- src/Polysemy.hs
+++ src/Polysemy.hs
@@ -23,8 +23,8 @@ module Polysemy
     --
     -- @
     -- data Console m a where
-    --   WriteLine :: 'String' -> Console m ()
-    --   ReadLine  :: Console m 'String'
+    --   WriteLine :: String -> Console m ()
+    --   ReadLine  :: Console m String
     -- @
     --
     -- Notice that the @a@ parameter gets instantiated at the /desired return/
@@ -41,7 +41,7 @@ module Polysemy
     --
     -- @
     -- writeLine :: 'Member' Console r => String -> 'Sem' r ()
-    -- readLine  :: 'Member' Console r => 'Sem' r 'String'
+    -- readLine  :: 'Member' Console r => 'Sem' r String
     -- @
     --
     -- Effects which don't make use of the @m@ parameter are known as
