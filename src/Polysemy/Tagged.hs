daf0c10453e9b11ca094fc2e6e23424345877781
Sandy Maguire <sandy@sandymaguire.me>
2019-11-01 19:10:08 +0100

    Add View effect

--- src/Polysemy/Tagged.hs
+++ src/Polysemy/Tagged.hs
@@ -6,6 +6,7 @@ module Polysemy.Tagged
 
     -- * Actions
   , tag
+  , tagged
 
     -- * Interpretations
   , untag
@@ -21,6 +22,7 @@ import Polysemy.Internal.Union
 -- | An effect for annotating effects and disambiguating identical effects.
 newtype Tagged k e m a where
   Tagged :: forall k e m a. e m a -> Tagged k e m a
+
 
 ------------------------------------------------------------------------------
 -- | Tag uses of an effect, effectively gaining access to the
@@ -40,7 +42,8 @@ newtype Tagged k e m a where
 --   'tag' @k @('Polysemy.Reader.Reader' i) $ 'Polysemy.Reader.local' @i f ('raise' m)
 -- @
 --
-tag :: forall k e r a
+tag
+    :: forall k e r a
      . Member (Tagged k e) r
     => Sem (e ': r) a
     -> Sem r a
@@ -50,11 +53,28 @@ tag = hoistSem $ \u -> case decomp u of
   Left g -> hoist (tag @k) g
 {-# INLINE tag #-}
 
+
+------------------------------------------------------------------------------
+-- | A reinterpreting version of 'tag'.
+tagged
+    :: forall k e r a
+     . Sem (e ': r) a
+    -> Sem (Tagged k e ': r) a
+tagged = hoistSem $ \u ->
+  case decompCoerce u of
+    Right (Weaving e s wv ex ins) ->
+      injWeaving $ Weaving (Tagged @k e) s (tagged @k . wv) ex ins
+    Left g -> hoist (tagged @k) g
+{-# INLINE tagged #-}
+
+
+
 ------------------------------------------------------------------------------
 -- | Run a @'Tagged' k e@ effect through reinterpreting it to @e@
-untag :: forall k e r a
-       . Sem (Tagged k e ': r) a
-      -> Sem (e ': r) a
+untag
+    :: forall k e r a
+     . Sem (Tagged k e ': r) a
+    -> Sem (e ': r) a
 -- TODO(KingoftheHomeless): I think this is safe to replace with 'unsafeCoerce',
 -- but doing so probably worsens performance, as it hampers optimizations.
 -- Once GHC 8.10 rolls out, I will benchmark and compare.
@@ -64,12 +84,14 @@ untag = hoistSem $ \u -> case decompCoerce u of
   Left g -> hoist untag g
 {-# INLINE untag #-}
 
+
 ------------------------------------------------------------------------------
 -- | Transform a @'Tagged' k1 e@ effect into a @'Tagged' k2 e@ effect
-retag :: forall k1 k2 e r a
-       . Member (Tagged k2 e) r
-      => Sem (Tagged k1 e ': r) a
-      -> Sem r a
+retag
+    :: forall k1 k2 e r a
+     . Member (Tagged k2 e) r
+    => Sem (Tagged k1 e ': r) a
+    -> Sem r a
 retag = hoistSem $ \u -> case decomp u of
   Right (Weaving (Tagged e) s wv ex ins) ->
     injWeaving $ Weaving (Tagged @k2 e) s (retag @_ @k2 . wv) ex ins
