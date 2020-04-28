4eece51af611e86fd24f2b1c50cfe352b61ff1f5
Georgi Lyubenov <godzbanebane@gmail.com>
2020-04-28 21:20:12 +0300

    ifdef the 810 parts so we still build on older ghcs

--- polysemy-plugin/src/Polysemy/Plugin/Fundep/Unification.hs
+++ polysemy-plugin/src/Polysemy/Plugin/Fundep/Unification.hs
@@ -1,9 +1,15 @@
+{-# LANGUAGE CPP                       #-}
+
 module Polysemy.Plugin.Fundep.Unification where
 
 import           Data.Bool
 import           Data.Function (on)
 import qualified Data.Set as S
+#if __GLASGOW_HASKELL__ >= 810
 import           Constraint
+#else
+import           TcRnTypes
+#endif
 
 import           Type
 
