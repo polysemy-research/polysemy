0614f102b0150ca0b68d4a1c5e7170a9cb15cb44
Gabriel Gonzalez <Gabriel439@gmail.com>
2021-04-23 11:34:56 -0700

    Add `Semigroup` and `Monoid` instances for `Sem`

--- src/Polysemy/Internal.hs
+++ src/Polysemy/Internal.hs
@@ -307,6 +307,11 @@ instance (Member Fail r) => MonadFail (Sem r) where
   fail = send . Fail
   {-# INLINE fail #-}
 
+instance Semigroup a => Semigroup (Sem f a) where
+  (<>) = liftA2 (<>)
+
+instance Monoid a => Monoid (Sem f a) where
+  mempty = pure mempty
 
 ------------------------------------------------------------------------------
 -- | This instance will only lift 'IO' actions. If you want to lift into some
