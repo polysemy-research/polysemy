d5e93379a48c4b1facb851fce0d7920e59f445e6
Sandy Maguire <sandy@sandymaguire.me>
2021-10-20 17:54:12 -0700

    Polymorphic and MTPC tests

--- polysemy-plugin/test/AmbiguousSpec.hs
+++ polysemy-plugin/test/AmbiguousSpec.hs
@@ -1,7 +1,9 @@
-{-# LANGUAGE TemplateHaskell             #-}
-{-# OPTIONS_GHC -fdefer-type-errors      #-}
+{-# LANGUAGE AllowAmbiguousTypes   #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
+{-# OPTIONS_GHC -fdefer-type-errors            #-}
 {-# OPTIONS_GHC -fno-warn-deferred-type-errors #-}
-{-# OPTIONS_GHC -fplugin=Polysemy.Plugin #-}
+{-# OPTIONS_GHC -fplugin=Polysemy.Plugin       #-}
 
 module AmbiguousSpec where
 
@@ -14,12 +16,24 @@ import Polysemy.State
 import Test.Hspec
 import Test.ShouldNotTypecheck
 
+class MPTC a b where
+  mptc :: a -> b
+
+instance MPTC Bool Int where
+  mptc _ = 1000
+
 
 uniquelyInt :: Members '[State Int , State String] r => Sem r ()
 uniquelyInt = put 10
 
+uniquelyA :: forall a b r. (Num a, Members '[State a, State b] r) => Sem r ()
+uniquelyA = put 10
+
 uniquelyString :: Members '[State Int , State String] r => Sem r ()
 uniquelyString = put mempty
+
+uniquelyB :: (MPTC Bool b, Members '[State String, State b] r) => Sem r ()
+uniquelyB = put $ mptc False
 
 uniquelyIO :: Members '[Embed IO, Embed Identity] r => Sem r ()
 uniquelyIO = embed $ liftIO $ pure ()
@@ -36,6 +50,14 @@ spec = describe "example" $ do
   it "should run uniquelyInt" $ do
     let z = run . runState 0 . runState "hello" $ uniquelyInt
     z `shouldBe` (10, ("hello", ()))
+
+  it "should run uniquelyA" $ do
+    let z = run . runState 0 . runState "hello" $ uniquelyA @Int @String
+    z `shouldBe` (10, ("hello", ()))
+
+  it "should run uniquelyB" $ do
+    let z = run . runState 0 . runState "hello" $ uniquelyB @Int
+    z `shouldBe` (1000, ("hello", ()))
 
   it "should run uniquelyString" $ do
     let z = run . runState 0 . runState "hello" $ uniquelyString
