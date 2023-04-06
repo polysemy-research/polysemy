9ec6e337d06f2936ba9c8e098757594ee0f383f9
Torsten Schmits <git@tryp.io>
2023-04-06 20:22:42 +0200

    support GHC 9.6

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -4,16 +4,23 @@
   compiler,
 }:
 let
-  overrides = pkgs: self: super:
+  pkgs = import nixpkgs { inherit system; };
+
+  overrides = self: super:
   let
     filter = pkgs.nix-gitignore.gitignoreSourcePure [./source-filter];
 
     c2n = name: src: pkgs.haskell.lib.disableLibraryProfiling (self.callCabal2nix name (filter src) {});
+
   in {
     polysemy = c2n "polysemy" ../.;
     polysemy-plugin = c2n "polysemy-plugin" ../polysemy-plugin;
+  } // pkgs.lib.optionalAttrs (compiler == "ghc96") {
+    type-errors = self.callHackageDirect {
+      pkg = "type-errors";
+      ver = "0.2.0.2";
+      sha256 = "sha256-z/QANg5jUhSc02IBVQEsg7QemiyV3XdwxN9K1CH2Myc=";
+    } {};
   };
-
-  pkgs = import nixpkgs { inherit system; };
 in
-  pkgs.haskell.packages.${compiler}.override { overrides = overrides pkgs; } // { inherit pkgs; }
+  pkgs.haskell.packages.${compiler}.override { inherit overrides; } // { inherit pkgs; }
